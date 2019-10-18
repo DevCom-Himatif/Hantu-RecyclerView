36f6cb8262382d038dd97dc82cce6cd2bb4904af
muhamistau <islam.taufikurahman@gmail.com>
2019-10-18 17:10:25 +0700

    update

--- app/src/main/java/com/muhamistau/recyclerapplication/util/HantuAdapter.kt
+++ app/src/main/java/com/muhamistau/recyclerapplication/util/HantuAdapter.kt
@@ -4,25 +4,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import com.muhamistau.recyclerapplication.R
+import com.muhamistau.recyclerapplication.model.Hantu
 
-//class HantuAdapter() : RecyclerView.Adapter<HantuAdapter.ViewHolder> {
+//class HantuAdapter(private val list: ArrayList<Hantu>) : RecyclerView.Adapter<HantuAdapter.ViewHolder> {
 //
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RvViewHolder {
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 //        val view = LayoutInflater.from(parent.context)
-//            .inflate(R.layout.rv_viewholder, parent, false)
-//        return RvViewHolder(view)
+//            .inflate(R.layout.item_hantu, parent, false)
+//        return ViewHolder(view)
 //    }
 //
 //    override fun getItemCount(): Int = list.size
 //
-//    override fun onBindViewHolder(holder: RvViewHolder, position: Int) {
+//    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 //        holder.bind(list[position])
 //    }
 //
-//    class RvViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-//        val textView = itemView.findViewById<TextView>(R.id.rv_textview)
-//        fun bind(item: Item){
-//            textView.text = item.value
+//    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+//        val textView = itemView.findViewById<TextView>(R.id.namaHantu)
+//        fun bind(hantu: Hantu){
+//            textView.text = hantu.nama
 //        }
 //    }
 //
