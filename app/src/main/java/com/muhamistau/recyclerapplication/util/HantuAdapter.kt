0b4461ad1a4ede671149251a674e91561cd22ac1
muhamistau <islam.taufikurahman@gmail.com>
2019-10-18 17:38:34 +0700

    update Adapter

--- app/src/main/java/com/muhamistau/recyclerapplication/util/HantuAdapter.kt
+++ app/src/main/java/com/muhamistau/recyclerapplication/util/HantuAdapter.kt
@@ -1,13 +1,19 @@
 package com.muhamistau.recyclerapplication.util
 
+import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import com.muhamistau.recyclerapplication.R
 import com.muhamistau.recyclerapplication.model.Hantu
 
-//class HantuAdapter(private val list: ArrayList<Hantu>) : RecyclerView.Adapter<HantuAdapter.ViewHolder> {
+//class HantuAdapter(
+//    private val context: Context,
+//    private val list: ArrayList<Hantu>
+//) : RecyclerView.Adapter<HantuAdapter.ViewHolder> {
 //
 //    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 //        val view = LayoutInflater.from(parent.context)
@@ -18,13 +24,17 @@ import com.muhamistau.recyclerapplication.model.Hantu
 //    override fun getItemCount(): Int = list.size
 //
 //    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-//        holder.bind(list[position])
+//        val currentHantu = list[position]
+//        holder.namaTextView.text = currentHantu.nama
+//        holder.bind(currentHantu) {
+//            Toast.makeText(context, "${currentHantu.nama} clicked", Toast.LENGTH_SHORT).show()
+//        }
 //    }
 //
-//    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-//        val textView = itemView.findViewById<TextView>(R.id.namaHantu)
-//        fun bind(hantu: Hantu){
-//            textView.text = hantu.nama
+//    inner class ViewHolder(private val itemView: View) : RecyclerView.ViewHolder(itemView) {
+//        val namaTextView: TextView = itemView.namaHantu
+//        fun bind(hantu: Hantu, listener: (Hantu) -> Unit) {
+//                itemView.setOnClickListener { listener(hantu) }
 //        }
 //    }
 //
